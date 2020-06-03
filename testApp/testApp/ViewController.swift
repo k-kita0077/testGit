c9c529e0d1a30863d1e2ef9741a1f4c07630b0e1
k_kita <ak2205d@icloud.com>
2020-06-03 23:18:30 +0900

    1st commit

---
+++ testApp/testApp/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  testApp
+//
+//  Created by kita kensuke on 2020/06/03.
+//  Copyright © 2020 kita kensuke. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
